27665926e58e0cbbeb82452b8edd144e62b39639
dylanbiesmans <dylan.biesmans@student.ucll.be>
2026-03-24 21:09:27 +0100

    Fix MainWindow.xaml

--- oefening2.1_testcode/UnitTest1.cs
+++ oefening2.1_testcode/UnitTest1.cs
@@ -5,10 +5,9 @@ using oefening2._1_startercode;
 namespace TestProject1
 {
     [TestFixture]
-    [Apartment(System.Threading.ApartmentState.STA)] // Belangrijk voor WPF!
+    [Apartment(System.Threading.ApartmentState.STA)] 
     public class Tests
     {
-        // 1. Hier vertel je de klasse dat er een _window bestaat
         private MainWindow _window;
 
         [SetUp]
@@ -25,12 +24,11 @@ namespace TestProject1
         [Test]
         public void Opgave_A_Test()
         {
-            // Nu zal _window wel herkend worden!
             _window.btnOne.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
 
             Assert.That(_window.lblOne.Content.ToString(), Is.EqualTo("Ja"));
             Assert.That(_window.lblTwo.Content.ToString(), Is.EqualTo("Ja"));
-            Assert.That(_window.lblThree.Content.ToString(), Is.EqualTo("Jaa"));
+            Assert.That(_window.lblThree.Content.ToString(), Is.EqualTo("Ja"));
         }
     }
 }
